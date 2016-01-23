08bbf267c18c90189591a07113b524ac4a0fc2ba
Zach Cole <zach.a.cole@gmail.com>
2016-01-23 13:44:25 -0800

    Changed Copyright information

--- tip-app/ViewController.swift
+++ tip-app/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var peopleControl: UISegmentedControl!
+    @IBOutlet weak var perPersonLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        perPersonLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onBillChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.2, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var peopleNumbers = [1.0, 2.0, 3.0, 4.0, 5.0]
+        var peopleNumber = peopleNumbers[peopleControl.selectedSegmentIndex]
+        
+        
+        var billAmount = NSString(string: BillField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        var perPersonTotal = total / peopleNumber
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        perPersonLabel.text = String(format: "$%.2f", perPersonTotal)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+        
+    }
 }
 
